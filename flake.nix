3c03f237fbd6d43c536bc0303986023cd06e9405
Aly Raffauf <aly@raffauflabs.com>
2024-07-06 17:58:23 -0400

    added outputs for common modules

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Opinionated hardware configuration modules for NixOS";
+  description = "Opinionated hardware configuration modules for NixOS.";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
@@ -19,6 +19,16 @@
       framework-13-intel-11th = import ./framework/13/intel-11th;
       lenovo-yoga-9i-intel-13th = import ./lenovo/yoga-9i/intel-13th;
       thinkpad-t440p = import ./thinkpad/t440p;
+
+      common-amd-cpu = import ./common/gpu/amd;
+      common-amd-gpu = import ./common/cpu/amd;
+      common-bluetooth = import ./common/bluetooth;
+      common-intel-cpu = import ./common/cpu/intel;
+      common-intel-gpu = import ./common/gpu/intel;
+      common-laptop = import ./common/laptop;
+      common-laptop-amd-gpu = import ./common/laptop/amd-gpu.nix;
+      common-laptop-intel-cpu = import ./common/laptop/intel-cpu.nix;
+      common-ssd = import ./common/ssd;
     };
   };
 }
