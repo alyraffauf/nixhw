fd51ff58018bcccd22a4df9acf60bece4f8d5d93
Aly Raffauf <aly@raffauflabs.com>
2024-09-14 17:21:19 -0400

    laptop/amd-gpu: remove abm level override

--- common/laptop/amd-gpu.nix
+++ common/laptop/amd-gpu.nix
@@ -1,12 +1,2 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  boot = {
-    # Disable AMD Backlight Management.
-    # ABM severely degrades display quality for miniscule power efficiency gains.
-    kernelParams = ["amdgpu.abmlevel=0"];
-  };
+{...}: {
 }
